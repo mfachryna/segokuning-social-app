d29b527ead923790b0df65fadbc59cb33c084933
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-19 02:48:31 +0700

    rerfactor (app): define pgx, validator and route

--- internal/app/app.go
+++ internal/app/app.go
@@ -10,20 +10,38 @@ import (
 	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
 	"github.com/shafaalafghany/segokuning-social-app/config"
+	"github.com/shafaalafghany/segokuning-social-app/internal/common/utils/validation"
+	handler "github.com/shafaalafghany/segokuning-social-app/internal/handler/user"
+	"github.com/shafaalafghany/segokuning-social-app/internal/repository"
 	"github.com/shafaalafghany/segokuning-social-app/pkg/db"
 )
 
 func Run(cfg *config.Configuration) {
-	db.NewPsqlDB(cfg)
+	var validate *validator.Validate
+
+	pgx := db.NewPsqlDB(cfg)
+
+	ur := repository.NewUserRepo(pgx)
+
+	validate = validator.New()
+	if err := validation.RegisterCustomValidation(validate); err != nil {
+		log.Fatalf("error register custom validation")
+	}
 
 	r := chi.NewRouter()
+
+	r.Route("/v1", func(r chi.Router) {
+		handler.NewUserHandler(r, ur, validate, *cfg)
+	})
+
 	s := &http.Server{
 		Addr:    cfg.Server.Port,
 		Handler: r,
 	}
 	go func() {
-		fmt.Println("Listen and Serve at port 8000")
+		fmt.Println("Listen and Serve at port 8080")
 		if err := s.ListenAndServe(); err != nil {
 			log.Fatalf("error in ListenAndServe: %s", err)
 		}
