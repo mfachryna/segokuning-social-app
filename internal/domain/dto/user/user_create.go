cab2310b01d10d68f4b0216f057795f0f342acc5
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-19 02:43:38 +0700

    refactor (user_create): add new struct for email and phone data

--- internal/domain/dto/user/user_create.go
+++ internal/domain/dto/user/user_create.go
@@ -1,8 +1,23 @@
 package dto
 
+import "time"
+
 type UserCreate struct {
-	CredentialType  string `json:"credentialType" validate:"required,eq=email|eq=phone"`
-	CredentialValue string `json:"credentialValue" validate:"required"`
-	Name            string `json:"name" validate:"required,min=5,max=50"`
-	password        string `json:"password" validate:"required,min=5,max=15"`
+	CredentialType  string    `json:"credentialType" validate:"required,eq=email|eq=phone"`
+	CredentialValue string    `json:"credentialValue" validate:"required"`
+	Name            string    `json:"name" validate:"required,min=5,max=50"`
+	Password        string    `json:"password" validate:"required,min=5,max=15"`
+	CreatedAt       time.Time `json:"createdAt"`
+}
+
+type EmailData struct {
+	Email       string `json:"email"`
+	Name        string `json:"name"`
+	AccessToken string `json:"accessToken"`
+}
+
+type PhoneData struct {
+	Phone       string `json:"phone"`
+	Name        string `json:"name"`
+	AccessToken string `json:"accessToken"`
 }
