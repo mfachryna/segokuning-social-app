b6a2b297f40788b0f03015f6b4f9004a51707ce6
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-19 02:47:25 +0700

    feat (register): add endpoint for register user

--- internal/handler/user/user_handler.go
+++ internal/handler/user/user_handler.go
@@ -3,16 +3,22 @@ package handler
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
+	"github.com/shafaalafghany/segokuning-social-app/config"
 	interfaces "github.com/shafaalafghany/segokuning-social-app/internal/interfaces"
 )
 
 type UserHandler struct {
-	ur        interfaces.UserRepository
-	validator *validator.Validate
+	ur  interfaces.UserRepository
+	val *validator.Validate
+	cfg config.Configuration
 }
 
-func NewUserHandler(r chi.Router, ur interfaces.UserRepository, val *validator.Validate) {
-	uh := &UserHandler{ur, val}
+func NewUserHandler(r chi.Router, ur interfaces.UserRepository, val *validator.Validate, cfg config.Configuration) {
+	uh := &UserHandler{
+		ur:  ur,
+		val: val,
+		cfg: cfg,
+	}
 
-	r.Post("/", uh.Register)
+	r.Post("/user/register", uh.Register)
 }
--- internal/handler/user/user_register.go
+++ internal/handler/user/user_register.go
@@ -2,92 +2,173 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"github.com/shafaalafghany/segokuning-social-app/internal/common/response"
 	"github.com/shafaalafghany/segokuning-social-app/internal/common/utils/validation"
+	dto "github.com/shafaalafghany/segokuning-social-app/internal/domain/dto/user"
+	"github.com/shafaalafghany/segokuning-social-app/internal/entity"
+	"github.com/shafaalafghany/segokuning-social-app/pkg/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func (uh *UserHandler) Register(w http.ResponseWriter, r *http.Request) {
-	var registerData domain.UserRegister
-	if err := json.NewDecoder(r.Body).Decode(&registerData); err != nil {
-		fmt.Println(err.Error())
+	var (
+		data    dto.UserCreate
+		resData interface{}
+	)
+
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
 		(&response.Response{
 			HttpStatus: http.StatusBadRequest,
-			Message:    response.ValidationAndParseBodyError,
+			Message:    "required fields are missing or invalid",
 		}).GenerateResponse(w)
 		return
 	}
 
-	if err := uh.validator.Struct(registerData); err != nil {
+	if err := uh.val.Struct(data); err != nil {
 		validationErrors := err.(validator.ValidationErrors)
 		for _, e := range validationErrors {
-			fmt.Println(err.Error())
 			(&response.Response{
 				HttpStatus: http.StatusBadRequest,
 				Message:    validation.CustomError(e),
 			}).GenerateResponse(w)
+			return
 		}
 	}
 
-	// registerData.Username = strings.ToLower(registerData.Username)
+	if data.CredentialValue == "" {
+		(&response.Response{
+			HttpStatus: http.StatusBadRequest,
+			Message:    "required fields are missing or invalid",
+		}).GenerateResponse(w)
+		return
+	}
 
-	var count int
-	err := uh.db.QueryRow("SELECT COUNT(*) FROM users WHERE username = $1", registerData.Username).Scan(&count)
+	ctx := r.Context()
+	uuid := uuid.NewString()
+	credType := data.CredentialType
+	user := entity.User{
+		ID:        uuid,
+		Name:      data.Name,
+		CreatedAt: time.Now(),
+	}
 
+	tokenString, err := jwt.SignedToken(jwt.Claim{UserId: uuid})
 	if err != nil {
-		fmt.Println(err.Error())
-		response.Error(w, apierror.CustomServerError(err.Error()))
+		(&response.Response{
+			HttpStatus: http.StatusInternalServerError,
+			Message:    err.Error(),
+		}).GenerateResponse(w)
 		return
 	}
 
-	if count > 0 {
-		err := apierror.ClientAlreadyExists()
-		fmt.Println(err.Message)
-		response.Error(w, err)
-		return
+	if credType == "phone" {
+		if err := validation.EmailValidation(data.CredentialValue); err != nil {
+			(&response.Response{
+				HttpStatus: http.StatusBadRequest,
+				Message:    err.Error(),
+			}).GenerateResponse(w)
+			return
+		}
+
+		count, err := uh.ur.PhoneCheck(ctx, data.CredentialValue)
+		if err != nil {
+			(&response.Response{
+				HttpStatus: http.StatusInternalServerError,
+				Message:    err.Error(),
+			}).GenerateResponse(w)
+			return
+		}
+
+		if count > 0 {
+			(&response.Response{
+				HttpStatus: http.StatusConflict,
+				Message:    "email is already used",
+			}).GenerateResponse(w)
+			return
+		}
+
+		user.Phone = data.CredentialValue
+
+		resData = dto.PhoneData{
+			Phone:       user.Phone,
+			Name:        user.Name,
+			AccessToken: tokenString,
+		}
+	} else {
+		if err := validation.EmailValidation(data.CredentialValue); err != nil {
+			(&response.Response{
+				HttpStatus: http.StatusBadRequest,
+				Message:    err.Error(),
+			}).GenerateResponse(w)
+			return
+		}
+
+		count, err := uh.ur.EmailCheck(ctx, data.CredentialValue)
+		if err != nil {
+			(&response.Response{
+				HttpStatus: http.StatusInternalServerError,
+				Message:    err.Error(),
+			}).GenerateResponse(w)
+			return
+		}
+
+		if count > 0 {
+			(&response.Response{
+				HttpStatus: http.StatusConflict,
+				Message:    "email is already used",
+			}).GenerateResponse(w)
+			return
+		}
+
+		user.Email = data.CredentialValue
+		resData = dto.PhoneData{
+			Phone:       user.Email,
+			Name:        user.Name,
+			AccessToken: tokenString,
+		}
 	}
 
-	date := time.Now()
+	salt, err := strconv.Atoi(uh.cfg.App.BcryptSalt)
+	if err != nil {
+		(&response.Response{
+			HttpStatus: http.StatusInternalServerError,
+			Message:    err.Error(),
+		}).GenerateResponse(w)
+		return
+	}
 
 	hashedPasswordChan := make(chan string)
 	go func() {
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registerData.Password), bcrypt.DefaultCost)
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(data.Password), salt)
 		if err != nil {
-			fmt.Println(err.Error())
-			response.Error(w, apierror.CustomServerError(err.Error()))
+			(&response.Response{
+				HttpStatus: http.StatusConflict,
+				Message:    "email is already used",
+			}).GenerateResponse(w)
 			return
 		}
 		hashedPasswordChan <- string(hashedPassword)
 	}()
 
-	var id string
-	uuid := uuid.New()
-	if err := uh.db.QueryRow(`INSERT INTO users (id,username,name,password,created_at,updated_at) VALUES ($1,$2,$3,$4,$5,$6) RETURNING id`, uuid, registerData.Username, registerData.Name, <-hashedPasswordChan, date, date).Scan(&id); err != nil {
-		fmt.Println(err.Error())
-		response.Error(w, apierror.CustomServerError(err.Error()))
+	user.Password = <-hashedPasswordChan
+
+	if err := uh.ur.Insert(ctx, user, credType); err != nil {
+		(&response.Response{
+			HttpStatus: http.StatusInternalServerError,
+			Message:    err.Error(),
+		}).GenerateResponse(w)
 		return
 	}
 
-	tokenString, err := jwt.SignedToken(jwt.Claim{
-		UserId: id,
-	})
-	if err != nil {
-		fmt.Println(err.Error())
-		response.Error(w, apierror.CustomServerError("Failed to generate access token"))
-		return
-	}
-
-	res := &domain.UserAuthResponse{
-		Name:        registerData.Name,
-		Username:    registerData.Username,
-		AccessToken: tokenString,
-	}
-
-	response.Success(w, apisuccess.RegisterResponse(res))
+	(&response.Response{
+		HttpStatus: http.StatusCreated,
+		Message:    "User registered successfully",
+		Data:       resData,
+	}).GenerateResponse(w)
 }
