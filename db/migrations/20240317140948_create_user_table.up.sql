dcf41cf0bd8b90f00702bda7fe40a65097fa8707
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-19 02:30:58 +0700

    refactor (migration users): add name field

--- db/migrations/20240317140948_create_user_table.up.sql
+++ db/migrations/20240317140948_create_user_table.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY NOT NULL,
-    email VARCHAR NOT NULL,
-    phone VARCHAR NOT NULL,
+    email VARCHAR,
+    phone VARCHAR,
+    name VARCHAR,
     password VARCHAR NOT NULL,
-    image_url VARCHAR NOT NULL,
+    image_url VARCHAR,
     friend_count INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL
