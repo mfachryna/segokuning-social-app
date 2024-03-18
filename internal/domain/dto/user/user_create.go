6a4788f469715b7edd186964019f238ddb3c82b6
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-18 22:19:33 +0700

    refactor (user_create): add field user create on dto

--- internal/domain/dto/user/user_create.go
+++ internal/domain/dto/user/user_create.go
@@ -1,4 +1,8 @@
 package dto
 
 type UserCreate struct {
+	CredentialType  string `json:"credentialType" validate:"required,eq=email|eq=phone"`
+	CredentialValue string `json:"credentialValue" validate:"required"`
+	Name            string `json:"name" validate:"required,min=5,max=50"`
+	password        string `json:"password" validate:"required,min=5,max=15"`
 }
