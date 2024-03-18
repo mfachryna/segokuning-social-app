7de9439aac0d2655c89b11546f63675a0607dd57
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-18 21:09:10 +0700

    feat (migration): add migration for friends table

---
+++ db/migrations/20240318082210_create_friends_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS friends {
+  id UUID PRIMARY KEY,
+  user_id UUID REFERENCES users(id) NOT NULL,
+  friend_id UUID REFERENCES users(id) NOT NULL,
+  created_at TIMESTAMP NOT NULL
+}
