50c3d1fa977390668ac8ecd2349fbd0b2434c66e
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-23 04:00:04 +0700

    feat (migration): add indexing for friends table

---
+++ db/migrations/20240322204828_create_index_friends_table.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX friends_friend_id;
+DROP INDEX friends_user_id;
---
+++ db/migrations/20240322204828_create_index_friends_table.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS friends_friend_id ON friends(friend_id);
+CREATE INDEX IF NOT EXISTS friends_user_id ON friends(user_id);
