6354670ddde98b000b248029d232247cda557fee
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-23 05:01:00 +0700

    refactor (create comment): implement logging

--- internal/handler/comment/comment_create.go
+++ internal/handler/comment/comment_create.go
@@ -11,6 +11,7 @@ import (
 	"github.com/shafaalafghany/segokuning-social-app/internal/common/utils/validation"
 	dto "github.com/shafaalafghany/segokuning-social-app/internal/domain/dto/comment"
 	"github.com/shafaalafghany/segokuning-social-app/internal/entity"
+	"go.uber.org/zap"
 )
 
 func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +21,7 @@ func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) 
 	)
 
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		uh.log.Error("required fields are missing or invalid", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusBadRequest,
 			Message:    "required fields are missing or invalid",
@@ -30,6 +32,7 @@ func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) 
 	if err := uh.val.Struct(data); err != nil {
 		validationErrors := err.(validator.ValidationErrors)
 		for _, e := range validationErrors {
+			uh.log.Error(validation.CustomError(e), zap.Error(err))
 			(&response.Response{
 				HttpStatus: http.StatusBadRequest,
 				Message:    validation.CustomError(e),
@@ -39,6 +42,7 @@ func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) 
 	}
 
 	if err := validation.UuidValidation(data.PostId); err != nil {
+		uh.log.Error("failed to validate uuid", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusNotFound,
 			Message:    err.Error(),
@@ -53,6 +57,7 @@ func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) 
 	post, err := uh.pr.FindById(ctx, data.PostId)
 	if err != nil {
 		if err == pgx.ErrNoRows {
+			uh.log.Error("user is not found", zap.Error(err))
 			(&response.Response{
 				HttpStatus: http.StatusNotFound,
 				Message:    "Post not found",
@@ -60,6 +65,7 @@ func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) 
 			return
 		}
 
+		uh.log.Error("failed to get user", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusInternalServerError,
 			Message:    err.Error(),
@@ -69,6 +75,7 @@ func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) 
 
 	count, err := uh.fr.FindByRelation(ctx, userId, post.UserId)
 	if err != nil {
+		uh.log.Error("failed to get user relation", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusInternalServerError,
 			Message:    err.Error(),
@@ -77,6 +84,7 @@ func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) 
 	}
 
 	if count <= 0 {
+		uh.log.Error("you cannot commented because you are not friend with this user")
 		(&response.Response{
 			HttpStatus: http.StatusBadRequest,
 			Message:    "You cannot commented because you are not friend with this user",
@@ -92,6 +100,7 @@ func (uh *CommentHandler) CreateComment(w http.ResponseWriter, r *http.Request) 
 	}
 
 	if err := uh.cr.Insert(ctx, commentEntity); err != nil {
+		uh.log.Error("failed to insert data", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusInternalServerError,
 			Message:    err.Error(),
