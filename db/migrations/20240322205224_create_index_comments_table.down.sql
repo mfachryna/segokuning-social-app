79db8de83688356135998fa07c40a04b83480d9a
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-23 04:00:24 +0700

    feat (migration): add indexing for comments table

---
+++ db/migrations/20240322205224_create_index_comments_table.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX comments_user_id;
+DROP INDEX comments_post_id;
---
+++ db/migrations/20240322205224_create_index_comments_table.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS comments_user_id ON comments(user_id);
+CREATE INDEX IF NOT EXISTS comments_post_id ON comments(post_id);
