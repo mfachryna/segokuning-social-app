bae81940dddac705442126f7984a6b63fb43cc93
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-03-23 04:59:09 +0700

    refactor (get post): implement logging

--- internal/handler/post/post_get.go
+++ internal/handler/post/post_get.go
@@ -9,13 +9,16 @@ import (
 	"github.com/shafaalafghany/segokuning-social-app/internal/common/utils/validation"
 	metadto "github.com/shafaalafghany/segokuning-social-app/internal/domain/dto/meta"
 	postdto "github.com/shafaalafghany/segokuning-social-app/internal/domain/dto/post"
+	"go.uber.org/zap"
 )
 
 func (uh *PostHandler) GetPost(w http.ResponseWriter, r *http.Request) {
 	var (
 		filter postdto.PostFilter
 	)
+
 	if err := r.ParseForm(); err != nil {
+		uh.log.Error("failed to parse form", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusInternalServerError,
 			Message:    err.Error(),
@@ -24,6 +27,7 @@ func (uh *PostHandler) GetPost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := validation.ValidateParams(r, filter); err != nil {
+		uh.log.Error("failed to validate params", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusBadRequest,
 			Message:    err.Error(),
@@ -32,6 +36,7 @@ func (uh *PostHandler) GetPost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := schema.NewDecoder().Decode(&filter, r.Form); err != nil {
+		uh.log.Error("required fields are missing or invalid", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusBadRequest,
 			Message:    err.Error(),
@@ -42,6 +47,7 @@ func (uh *PostHandler) GetPost(w http.ResponseWriter, r *http.Request) {
 	if err := uh.val.Struct(filter); err != nil {
 		validationErrors := err.(validator.ValidationErrors)
 		for _, e := range validationErrors {
+			uh.log.Error(validation.CustomError(e), zap.Error(err))
 			(&response.Response{
 				HttpStatus: http.StatusBadRequest,
 				Message:    validation.CustomError(e),
@@ -60,6 +66,7 @@ func (uh *PostHandler) GetPost(w http.ResponseWriter, r *http.Request) {
 
 	data, count, err := uh.pr.GetPostWithFilter(ctx, filter, userId)
 	if err != nil {
+		uh.log.Error("failed to get post with filter", zap.Error(err))
 		(&response.Response{
 			HttpStatus: http.StatusInternalServerError,
 			Message:    err.Error(),
